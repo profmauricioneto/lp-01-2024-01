1c72d9ba52b87a05c420fad13a33eb0576f20a07
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2024-02-09 11:09:03 -0300

    add: initial java contents

---
+++ fundamentals/src/main/java/org/example/OutPutExamples.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class OutPutExamples {
+    public static void main(String[] args) {
+        float valorNota = 8.7f;
+        System.out.println("Sua nota é: " + valorNota);
+        System.out.println ("Término do programa.");
+    }
+}
