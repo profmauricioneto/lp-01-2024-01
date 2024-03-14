bab21a7b487602422d0a7f35127d0609db85c807
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2024-03-14 07:18:34 -0300

    add: file manipulation example.

--- fundamentals/src/main/java/org/filemanipulation/CreateFile.java
+++ fundamentals/src/main/java/org/filemanipulation/CreateFile.java
@@ -1,18 +1,15 @@
 package org.filemanipulation;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class CreateFile {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        File file;
-        String filename;
-        System.out.println("Digite o nome do seu arquivo: ");
-        filename = scan.next();
-        file = new File(filename.concat(".txt"));
 
+    public static void create(String filename) {
+        File file;
+        file = new File(filename.concat(".txt"));
         try {
             if(file.createNewFile()){
                 System.out.println("Arquivo Criado com Sucesso!" + file.getName());
@@ -20,5 +17,56 @@ public class CreateFile {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void writerInFile(String filename, String message) {
+        try {
+            FileWriter objWriter = new FileWriter(filename);
+            objWriter.write(message);
+            objWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void readerInFile(String filename) {
+        try {
+            File file = new File(filename.concat(".txt"));
+            Scanner reader = new Scanner(file);
+            while(reader.hasNextLine()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int option;
+
+        System.out.println("Digite uma opcao: ");
+        option = scan.nextInt();
+
+        switch (option) {
+            case 1: {
+                create("teste");
+                break;
+            }
+            case 2: {
+                writerInFile("teste", "ola");
+                break;
+            }
+            case 3: {
+                readerInFile("teste");
+                break;
+            }
+            default:
+                System.out.println("No options chooses");
+        }
+
+
     }
 }
