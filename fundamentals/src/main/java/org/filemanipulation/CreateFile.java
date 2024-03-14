c15973a231aa47935a6cf6ce2b6ded507574e2ce
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2024-03-14 13:27:28 -0300

    add: completed file manipulation code.

--- fundamentals/src/main/java/org/filemanipulation/CreateFile.java
+++ fundamentals/src/main/java/org/filemanipulation/CreateFile.java
@@ -4,34 +4,59 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-
+/**
+ * @author maumneto
+ * This code is an example of file manipulation in Java.
+ * Each subroutine executes an action in file created
+ * */
 public class CreateFile {
-
-    public static void create(String filename) {
-        File file;
-        file = new File(filename.concat(".txt"));
+    /**
+     * This method is responsible to print a menu
+     */
+    public static void menu() {
+        System.out.println("1 - Write in File");
+        System.out.println("2 - Read File");
+        System.out.println("3 - Delete File");
+    }
+    /**
+     * @param file
+     * This method is responsible to create a new File.
+     */
+    public static void createFile(File file) {
         try {
             if(file.createNewFile()){
-                System.out.println("Arquivo Criado com Sucesso!" + file.getName());
+                System.out.println("File Created with Success! Filename: " + file.getName());
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void writerInFile(String filename, String message) {
-        try {
-            FileWriter objWriter = new FileWriter(filename);
-            objWriter.write(message);
-            objWriter.close();
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+    /**
+     * @param file, message, appendable
+     * This method is responsible to write a content in file.
+     * */
+    public static void writeInFile(File file, String message, boolean appendable) {
+        if (file.exists()) {
+            try {
+                FileWriter objWriter = new FileWriter(file, appendable);
+                objWriter.write(message);
+                System.out.println("Success write in File!");
+                objWriter.close();
+            } catch (IOException e) {
+                System.out.println("Error to write in File.");
+                System.out.println("Error: " + e.getMessage());
+            }
+        } else {
+            System.out.println("File not Found.");
         }
     }
 
-    public static void readerInFile(String filename) {
+    /**
+     * @param file
+     * This method is responsible to read all content of file */
+    public static void readFile(File file) {
         try {
-            File file = new File(filename.concat(".txt"));
             Scanner reader = new Scanner(file);
             while(reader.hasNextLine()) {
                 String data = reader.nextLine();
@@ -40,31 +65,50 @@ public class CreateFile {
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-
     }
 
+    /**
+     * This method is responsible to delete file.
+     * */
+    public static void deleteFile(File file) {
+        if (file.delete()) {
+            System.out.println("File deleted with success.");
+        } else {
+            System.out.println("Error: delete option failed.");
+        }
+    }
+
+    /**
+     * main code
+     * */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int option;
+        File file = new File("teste.txt");
 
-        System.out.println("Digite uma opcao: ");
+        menu();
+        System.out.println("Option: ");
         option = scan.nextInt();
 
         switch (option) {
             case 1: {
-                create("teste");
+                System.out.print("Write your message: ");
+                String message = scan.nextLine().concat("\n");
+                System.out.print("Do you want to override the content of the file? [y/n]");
+                String choice = scan.next();
+                writeInFile(file, message, choice.contains("y"));
                 break;
             }
             case 2: {
-                writerInFile("teste", "ola");
+                readFile(file);
                 break;
             }
             case 3: {
-                readerInFile("teste");
+                deleteFile(file);
                 break;
             }
             default:
-                System.out.println("No options chooses");
+                System.out.println("No options choosen");
         }
 
 
