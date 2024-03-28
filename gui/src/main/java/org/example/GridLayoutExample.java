7369101751af72685e5770a81e194ad1529f3434
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2024-03-28 17:25:44 -0300

    add: gridlayout example.

---
+++ gui/src/main/java/org/example/GridLayoutExample.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+
+public class GridLayoutExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Example GridLayout");
+        JPanel panelSup = new JPanel();
+        JPanel panelInf = new JPanel();
+
+        JLabel lbIdade = new JLabel("Idade: ");
+        JLabel lbResultado = new JLabel("");
+        JTextField tfIdade = new JTextField("0", 30);
+        JButton btnCalcular = new JButton("Calcular");
+
+        lbIdade.setHorizontalAlignment(SwingConstants.CENTER);
+
+        panelSup.setLayout(new GridLayout(1,2));
+        panelSup.add(lbIdade);
+        panelSup.add(tfIdade);
+
+        panelInf.setLayout(new GridLayout(2,1));
+        panelInf.add(btnCalcular);
+        panelInf.add(lbResultado);
+
+        frame.setLayout(new GridLayout(2,1));
+        frame.add(panelSup);
+        frame.add(panelInf);
+
+        btnCalcular.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               int anoNascimento = Integer.parseInt(tfIdade.getText());
+               int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
+               int idade = anoAtual - anoNascimento;
+               lbResultado.setText("Sua idade é: "+ idade);
+            }
+        });
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
