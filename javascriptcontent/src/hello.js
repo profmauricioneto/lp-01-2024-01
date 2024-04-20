e7ae91e8db8fbdb45c91c2c8b5c3eeeae4bdd85f
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2024-04-20 12:07:58 -0300

    add: web code.

---
+++ javascriptcontent/src/hello.js
@@ -0,0 +1,25 @@
+
+{
+    let nome = "mauricio";
+    console.log(nome);
+}
+
+console.log("Hello world!");
+
+for (var i= 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log("Ultimo valor de i = " + i);
+
+console.log(parseFloat("123") + 0.5);
+
+var nota = 123.44;
+
+console.log(nota.toFixed(4));
+
+var nameUser = "fulano!";
+console.log(`Ei ${nameUser}
+tu ta acordado?
+Isso cai em prova hein!
+`);
